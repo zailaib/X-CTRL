b0441aec9659a8eee5db19e46e2bb8f92afaed84
linjie <linjie@moseeker.com>
2024-05-07 16:20:30 +0800

    add comment

--- Receivers/RCX/Software/RCX_GOODUSB/RCX_GOODUSB.ino
+++ Receivers/RCX/Software/RCX_GOODUSB/RCX_GOODUSB.ino
@@ -9,19 +9,25 @@ bool UseMtm = true;
 
 void setup()
 {
+  // 关闭调试端口
   disableDebugPorts();
 
   StorageDataReg(Sim_ObjectSelect);
   EEPROM_Handle(EEPROM_Chs::ReadData);
 
+ // 初始化设备
   Sim_DeviceInit();
 
+  // 设置引脚模式
   pinMode(LED_Pin, OUTPUT);
   pinMode(KEY_Pin, INPUT_PULLUP);
   pinMode(NRF_IRQ_Pin, INPUT_PULLUP);
   pinMode(RandomSeed_Pin, INPUT_ANALOG);
+  // led 闪烁5次
   __LoopExecute((togglePin(LED_Pin), delay(80)), 5);
+  // 产生随机数
   randomSeed(analogRead(RandomSeed_Pin) * 1000);
+  // 绑定引脚下降沿到函数用于设置当前的USB设备使用哪种模拟设备（键盘，鼠标，xbox）
   attachInterrupt(KEY_Pin, When_KeyPressEvent, FALLING);
 
   Com_Init();
@@ -51,5 +57,5 @@ static void When_KeyPressEvent()
 {
   Sim_SetNextObj();
   EEPROM_Handle(EEPROM_Chs::SaveData);
-  NVIC_SystemReset();
+  NVIC_SystemReset(); // 软件复位
 }
